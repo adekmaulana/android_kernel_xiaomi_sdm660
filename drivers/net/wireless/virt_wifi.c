2faaa731a098e5280941cabc2b199a2dd35fe698
Adek Maulana <adekzmaulana@gmail.com>
2018-12-14 09:28:51 +0700

    net: virt_wifi: re-renamed IEEE80211_BAND_xxGHZ -> NL80211_BAND_xxGHZ
    after 9456397cff0dd197e0c450a23d0ef848a51ab632
    
    Change-Id: I5a4f1ccec3db10ac864d8b26041dee8226b83322

--- drivers/net/wireless/virt_wifi.c
+++ drivers/net/wireless/virt_wifi.c
@@ -28,7 +28,7 @@ struct virt_wifi_wiphy_priv {
 };
 
 static struct ieee80211_channel channel_2ghz = {
-	.band = IEEE80211_BAND_2GHZ,
+	.band = NL80211_BAND_2GHZ,
 	.center_freq = 2432,
 	.hw_value = 2432,
 	.max_power = 20,
@@ -47,7 +47,7 @@ static struct ieee80211_rate bitrates_2ghz[] = {
 static struct ieee80211_supported_band band_2ghz = {
 	.channels = &channel_2ghz,
 	.bitrates = bitrates_2ghz,
-	.band = IEEE80211_BAND_2GHZ,
+	.band = NL80211_BAND_2GHZ,
 	.n_channels = 1,
 	.n_bitrates = ARRAY_SIZE(bitrates_2ghz),
 	.ht_cap = {
@@ -67,7 +67,7 @@ static struct ieee80211_supported_band band_2ghz = {
 };
 
 static struct ieee80211_channel channel_5ghz = {
-	.band = IEEE80211_BAND_5GHZ,
+	.band = NL80211_BAND_5GHZ,
 	.center_freq = 5240,
 	.hw_value = 5240,
 	.max_power = 20,
@@ -100,7 +100,7 @@ static struct ieee80211_rate bitrates_5ghz[] = {
 static struct ieee80211_supported_band band_5ghz = {
 	.channels = &channel_5ghz,
 	.bitrates = bitrates_5ghz,
-	.band = IEEE80211_BAND_5GHZ,
+	.band = NL80211_BAND_5GHZ,
 	.n_channels = 1,
 	.n_bitrates = ARRAY_SIZE(bitrates_5ghz),
 	.ht_cap = {
@@ -359,9 +359,9 @@ static struct wiphy *virt_wifi_make_wiphy(void)
 	wiphy->max_scan_ie_len = 1000;
 	wiphy->signal_type = CFG80211_SIGNAL_TYPE_MBM;
 
-	wiphy->bands[IEEE80211_BAND_2GHZ] = &band_2ghz;
-	wiphy->bands[IEEE80211_BAND_5GHZ] = &band_5ghz;
-	wiphy->bands[IEEE80211_BAND_60GHZ] = NULL;
+	wiphy->bands[NL80211_BAND_2GHZ] = &band_2ghz;
+	wiphy->bands[NL80211_BAND_5GHZ] = &band_5ghz;
+	wiphy->bands[NL80211_BAND_60GHZ] = NULL;
 
 	wiphy->regulatory_flags = REGULATORY_WIPHY_SELF_MANAGED;
 	wiphy->interface_modes = BIT(NL80211_IFTYPE_STATION);
